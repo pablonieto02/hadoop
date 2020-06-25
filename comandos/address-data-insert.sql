0614f718e4eeedae6bd77d1e2ffb567834ad17fc
Pablo <pablonieto02@gmail.com>
2020-06-25 09:49:11 -0300

    estudo

---
+++ comandos/address-data-insert.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO address (id, custumer_id, street1, street2, city, state, country, zip_pin_postal_code) VALUES (0, 0, 'd-40', 'chavez street', 'natal', 'rio grande do norte', 'brasil', '778908');
+INSERT INTO address (id, custumer_id, street1, street2, city, state, country, zip_pin_postal_code) VALUES (1, 1, 'l-90', 'cooper street', 'san jose', 'california', 'usa', '400056');
+INSERT INTO address (id, custumer_id, street1, street2, city, state, country, zip_pin_postal_code) VALUES (2, 2, 'a-47', 'sector-11', 'londres', 'londres', 'united kingdom', '201311');
+INSERT INTO address (id, custumer_id, street1, street2, city, state, country, zip_pin_postal_code) VALUES (3, 3, 'r-98', 'sector-37', 'recife', 'pernambuco', 'brasil', '122021');
+INSERT INTO address (id, custumer_id, street1, street2, city, state, country, zip_pin_postal_code) VALUES (4, 4, '201', 'high street', 'blumenau', 'santa catarina', 'brasil', '41101');
